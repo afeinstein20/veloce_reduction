fixed bug in process_whites() and make_master_calib() during the error calculation if n_files==1; also made variance calculation in optimal_extraction better deal with NaNs

process_scripts.py
@@ -176,7 +176,10 @@ def process_whites(white_list, MB=None, ronmask=None, MD=None, gain=None, P_id=N
         # if roughly Gaussian distribution of values: error of median ~= 1.253*error of mean
         # err_master = 1.253 * np.std(allimg, axis=0) / np.sqrt(nw-1)     # normally it would be sigma/sqrt(n), but np.std is dividing by sqrt(n), not by sqrt(n-1)
         # need to rescale by exp time here, too
-        err_master = 1.253 * np.std(np.array(allimg) / tscale.reshape(len(allimg), 1, 1), axis=0) / np.sqrt(nw-1)     # normally it would be sigma/sqrt(n), but np.std is dividing by sqrt(n), not by sqrt(n-1)
+        if nw == 1:
+            err_master = allerr[0]
+        else:
+            err_master = 1.253 * np.std(np.array(allimg) / tscale.reshape(len(allimg), 1, 1), axis=0) / np.sqrt(nw-1)     # normally it would be sigma/sqrt(n), but np.std is dividing by sqrt(n), not by sqrt(n-1)
         # err_master = np.sqrt( np.sum( (np.array(allimg) - np.mean(np.array(allimg), axis=0))**2 / (nw*(nw-1)) , axis=0) )   # that is equivalent, but slower
     
     
@@ -198,9 +201,9 @@ def process_whites(white_list, MB=None, ronmask=None, MD=None, gain=None, P_id=N
         # pyfits.setval(outfn, 'EXPTIME', value=texp, comment='exposure time [s]')
         pyfits.setval(outfn, 'UNITS', value='ELECTRONS')
         if fancy:
-            pyfits.setval(outfn, 'METHOD', value='fancy', comment='method to create master white and remove outliers')
+            pyfits.setval(outfn, 'METHOD', value='fancy', comment='method to create master white & remove outliers')
         else:
-            pyfits.setval(outfn, 'METHOD', value='median', comment='method to create master white and remove outliers')
+            pyfits.setval(outfn, 'METHOD', value='median', comment='method to create master white & remove outliers')
         h = pyfits.getheader(outfn)
         h_err = h.copy()
         h_err['HISTORY'] = 'estimated uncertainty in MASTER WHITE frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)'

veloce_reduction/calibration.py
@@ -1711,9 +1711,12 @@ def make_master_calib(file_list, lamptype=None, MB=None, ronmask=None, MD=None, 
 #         # estimate of the corresponding error array (estimate only!!!)
 #         err_master = err_summed / nw     # I don't know WTF I was thinking here...
     # if roughly Gaussian distribution of values: error of median ~= 1.253*error of mean
-    err_master = 1.253 * np.std(allimg, axis=0) / np.sqrt(nw-1)     # normally it would be sigma/sqrt(n), but np.std is dividing by sqrt(n), not by sqrt(n-1)
+    if nw == 1:
+        err_master = allerr[0]
+    else:
+        err_master = 1.253 * np.std(allimg, axis=0) / np.sqrt(nw-1)     # normally it would be sigma/sqrt(n), but np.std is dividing by sqrt(n), not by sqrt(n-1)
     # err_master = np.sqrt( np.sum( (np.array(allimg) - np.mean(np.array(allimg), axis=0))**2 / (nw*(nw-1)) , axis=0) )   # that is equivalent, but slower
-    
+
     
     # now subtract background (errors remain unchanged)
     if remove_bg:

veloce_reduction/extraction.py
@@ -565,8 +565,8 @@ def optimal_extraction(stripes, err_stripes=None, ron_stripes=None, slit_height=
                 # not sure if this is the proper way to do this, but we can't have negative variance
                 # v[np.logical_or(v<=0,f<=0)] = RON*RON
                 # v[v<RON*RON] = np.maximum(RON*RON,1.)   #just a stupid fix so that variance is never below 1
-                v[np.logical_or(v <= 0, f <= 0)] = np.mean(roncol) ** 2
-                v[v < np.mean(roncol) ** 2] = np.maximum(np.mean(roncol) ** 2, 1.)  # just a stupid fix so that variance is never below 1
+                v[np.logical_or(v <= 0, f <= 0)] = np.nanmean(roncol) ** 2
+                v[v < np.nanmean(roncol) ** 2] = np.maximum(np.nanmean(roncol) ** 2, 1.)  # just a stupid fix so that variance is never below 1
 
                 if individual_fibres:
                     ### THIS IS METHOD (3a) - PREFERRED OPTION! ###
@@ -893,8 +893,8 @@ def optimal_extraction_from_indices(img, stripe_indices, err_img=None, ronmask=N
                 # not sure if this is the proper way to do this, but we can't have negative variance
                 # v[np.logical_or(v<=0,f<=0)] = RON*RON
                 # v[v<RON*RON] = np.maximum(RON*RON,1.)   # just a stupid fix so that variance is never below 1
-                v[np.logical_or(v <= 0, f <= 0)] = np.mean(roncol) ** 2
-                v[v < np.mean(roncol) ** 2] = np.maximum(np.mean(roncol) ** 2, 1.)  # just a stupid fix so that variance is never below 1
+                v[np.logical_or(v <= 0, f <= 0)] = np.nanmean(roncol) ** 2
+                v[v < np.nanmean(roncol) ** 2] = np.maximum(np.nanmean(roncol) ** 2, 1.)  # just a stupid fix so that variance is never below 1
 
                 if individual_fibres:
                     ### THIS IS METHOD (3a) - PREFERRED OPTION! ###